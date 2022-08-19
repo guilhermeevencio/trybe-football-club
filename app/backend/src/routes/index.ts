37685c3ee1ee708d597767ae0957cd27b48dee07
Guilherme <guilhermeevencioluz@gmail.com>
2022-08-19 14:16:41 -0300

    feat: Adds the teams route to the routes folder.

--- app/backend/src/routes/index.ts
+++ app/backend/src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import LoginRoutes from './user.routes';
+import TeamsRoutes from './teams.routes';
 
 const router = Router();
 
 router.use(LoginRoutes);
+router.use(TeamsRoutes);
 
 export default router;
---
+++ app/backend/src/routes/teams.routes.ts
@@ -0,0 +1,7 @@
+import { Router } from 'express';
+
+const TeamsRoutes = Router();
+
+TeamsRoutes.get('/teams', (req, res, next) => {});
+
+export default TeamsRoutes;
