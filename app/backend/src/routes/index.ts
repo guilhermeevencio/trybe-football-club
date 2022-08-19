3e67136f6b1ab6deef96211a7de2d4083e24f076
Guilherme <guilhermeevencioluz@gmail.com>
2022-08-19 19:21:25 -0300

    feat: adds the matches file in the routes folder

--- app/backend/src/routes/index.ts
+++ app/backend/src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import LoginRoutes from './user.routes';
 import TeamsRoutes from './teams.routes';
+import MatchesRoutes from './matches.routes';
 
 const router = Router();
 
 router.use(LoginRoutes);
 router.use(TeamsRoutes);
+router.use(MatchesRoutes);
 
 export default router;
---
+++ app/backend/src/routes/matches.routes.ts
@@ -0,0 +1,5 @@
+import { Router } from 'express';
+
+const MatchesRoutes = Router();
+
+export default MatchesRoutes;
